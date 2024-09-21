5717f86888614243bb1dcceb00065bd638517f5c
Kariqs <kariukibenard189@gmail.com>
2024-09-21 18:05:41 +0300

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./views/**/*.ejs",
+  "./public/**/*.js",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
