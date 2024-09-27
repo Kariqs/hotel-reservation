c922a1480241852dec017dc44325cf13051feba6
Kariqs <kariukibenard189@gmail.com>
2024-09-27 12:40:25 +0300

    Worked on the book room feature.

--- api/index.js
+++ api/index.js
@@ -16,11 +16,11 @@ const __dirname = path.dirname(__filename);
 
 const MONGODB_URL =
   process.env.MONGODB_URL || "mongodb://localhost:27017/hotel-reservation";
-const PORT = process.env.PORT || 8080; // It's process.env.PORT, not process.env.port (case-sensitive)
+const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "../views"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
